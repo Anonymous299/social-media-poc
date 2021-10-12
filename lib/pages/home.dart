27ad532e4d3755f7a326950242af0e879bada4a3
Sahil Pradhan <sahilpradhan@Sahils-MacBook-Air.local>
2021-10-12 17:05:19 -0400

    Created sign in splash screen

---
+++ lib/pages/home.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  Home({Key? key}) : super(key: key);
+
+  @override
+  _HomeState createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  bool isAuth = false;
+
+  Widget buildAuthScreen() {
+    return Text("Home");
+  }
+
+  Scaffold buildUnAuthScreen() {
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+            gradient: LinearGradient(
+                begin: Alignment.topRight,
+                end: Alignment.bottomLeft,
+                colors: [Colors.blue, Colors.purple])),
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              "SocialMediaPOC",
+              style: TextStyle(
+                  fontFamily: "Signatra", color: Colors.white, fontSize: 60.0),
+            ),
+            GestureDetector(
+              onTap: () {
+                print("Tapped");
+              },
+              child: Container(
+                decoration: BoxDecoration(
+                  image: DecorationImage(
+                      image:
+                          AssetImage("assets/images/google_signin_button.png"),
+                      fit: BoxFit.cover),
+                ),
+                width: 260.0,
+                height: 60.0,
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return isAuth ? buildAuthScreen() : buildUnAuthScreen();
+  }
+}
